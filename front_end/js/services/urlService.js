088901672607150902a2c90f97353a763838dd69
pushnoy100500 <romanenko.igor2014@outlook.com>
2016-04-12 01:44:49 -0400

    yo bullshit

--- front_end/js/services/urlService.js
+++ front_end/js/services/urlService.js
@@ -1,13 +1,13 @@
 var app = angular.module("HueMeApp");
 app.service('urlService', function() {
-	this.recentPostsUrl = "http://localhost:8888/hueme/index.php/Posts/getRecent/6";
-	this.loginUrl = "http://localhost:8888/hueme/index.php/login";
-	this.registerUrl = "http://localhost:8888/hueme/index.php/register";
-	this.updateProfUrl = "http://localhost:8888/hueme/index.php/updateProfile";
-	this.leavePostUrl = "http://localhost:8888/hueme/index.php/post";
-	this.postsByUserUrl = "http://localhost:8888/hueme/index.php/Posts/getAllPostsByUser";
-	this.sendComment = "http://localhost:8888/hueme/index.php/reply";
-	this.commentsByPostUrl = "http://localhost:8888/hueme/index.php/Posts/getComments";
+	this.recentPostsUrl = "http://localhost:8888/hueme/";
+	this.loginUrl = "http://localhost:8888/hueme/login";
+	this.registerUrl = "http://localhost:8888/hueme/register";
+	this.updateProfUrl = "http://localhost:8888/hueme/updateProfile";
+	this.leavePostUrl = "http://localhost:8888/hueme/post";
+	this.postsByUserUrl = "http://localhost:8888/hueme/getAllPostsByUser";
+	this.sendComment = "http://localhost:8888/hueme/reply";
+	this.commentsByPostUrl = "http://localhost:8888/hueme/Posts/getComments";
 
 	// this url needs to be changed once there is a route for all posts
 	// using get posts by ten now

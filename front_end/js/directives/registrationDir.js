4cea3905bd40eadbb749ffa957919c01530aa453
pushnoy100500 <romanenko.igor2014@outlook.com>
2016-04-13 01:52:28 -0400

    registering existing user logic

--- front_end/js/directives/registrationDir.js
+++ front_end/js/directives/registrationDir.js
@@ -6,8 +6,11 @@ app.directive("registrationDir", function(regLogService, $state) {
 		// this is a relative path to the template from index.html file
 		templateUrl: "templates/registration.html",
 		controller: function(regLogService, $state){
+			var self = this;
 			this.formModel = {};
 			this.inputType = "password";
+			this.username_exist = false;
+			this.email_exist = false;
 
 			this.showPass = function(){
 				this.inputType = 'text';
@@ -18,11 +21,15 @@ app.directive("registrationDir", function(regLogService, $state) {
 			};
 
 			this.formSubmit = function(){
-			regLogService.registerUser(this.formModel, function(isComplete) {
-				if(isComplete) {
-					$state.go('myprofile');
+			regLogService.registerUser(this.formModel, function(user) {
+				console.dir(user);	
+				if(user.success) {
+					self.username_exist = false;
+					self.email_exist = false;
+					$state.go('myprofile');					
 				} else {
-					//console.log('fuuck');
+					self.username_exist = (user['username_exist'] == 0 ? false : true);
+					self.email_exist = (user['email_exist'] == 0 ? false : true);
 				}
 			});
 

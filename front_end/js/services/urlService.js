a3d9abb008d6e9c0eccc8a08a76bc5c7e9d3b329
pushnoy100500 <romanenko.igor2014@outlook.com>
2016-04-13 12:19:00 -0400

    updated url in url service

--- front_end/js/services/urlService.js
+++ front_end/js/services/urlService.js
@@ -11,7 +11,23 @@ app.service('urlService', function() {
 
 	// this url needs to be changed once there is a route for all posts
 	// using get posts by ten now
-	this.allPostsUrl = "http://localhost:8888/CIHueMe/index.php/postsByTen/0";
+	this.allPostsUrl = "http://localhost:8888/CIHueMe/index.php/allPosts";
 	this.aboutUrl = "http://localhost:8888/CIHueMe/index.php/about";
 	this.getProfileUrl = "http://localhost:8888/CIHueMe/index.php/getProfile/";
 });
+/*
+v	this.recentPostsUrl = "http://localhost:8888/CIHueMe/index.php/getRecent/6";
+	this.loginUrl = "http://localhost:8888/CIHueMe/index.php/login";
+	this.registerUrl = "http://localhost:8888/CIHueMe/index.php/register";
+	this.updateProfUrl = "http://localhost:8888/CIHueMe/index.php/updateProfile";
+	this.leavePostUrl = "http://localhost:8888/CIHueMe/index.php/post";
+	this.postsByUserUrl = "http://localhost:8888/CIHueMe/index.php/postsByUser/";
+	this.sendComment = "http://localhost:8888/CIHueMe/index.php/reply";
+	this.commentsByPostUrl = "http://localhost:8888/CIHueMe/index.php/Posts/getComments";
+
+	// this url needs to be changed once there is a route for all posts
+	// using get posts by ten now
+	this.allPostsUrl = "http://localhost:8888/CIHueMe/index.php/allPosts";
+	this.aboutUrl = "http://localhost:8888/CIHueMe/index.php/about";
+	this.getProfileUrl = "http://localhost:8888/CIHueMe/index.php/getProfile/";
+*/
